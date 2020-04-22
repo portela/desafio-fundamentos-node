b191fa8a20c771f7134112bbb6078404f76498ef
Henrique Portela <henrique.portela@gmail.com>
2020-04-22 17:55:59 -0300

    CreateTransactionService draft.

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
@@ -20,7 +20,17 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
-    const transaction = transactionsRepository.create({ title, value, type });
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
     return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
